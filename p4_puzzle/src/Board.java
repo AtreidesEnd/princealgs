48519f37070fc065b8476a34816978bf73c634d4
AtreidesEnd <jmeek13@gmail.com>
2017-06-11 14:24:57 +0100

    finished assignment4 - 8puzzle solver

--- p4_puzzle/src/Board.java
+++ p4_puzzle/src/Board.java
@@ -5,46 +5,32 @@ import edu.princeton.cs.algs4.StdOut;
  * Created by jmeek on 29/05/2017.
  */
 public class Board {
-    private int[][] blocks;
-    private int[][] twin;
-    private int n;
-    private int empRow;
-    private int empCol;
+    private short[][] blocks;
+    private short n;
+    private short empRow;
+    private short empCol;
     private int hammingD;
     private int manhattanD;
 
     // construct a board from an n-by-n array of blocks
     // (where blocks[i][j] = block in row i, column j)
     public Board(int[][] blocks) {
-        this.n = blocks.length;
-        this.blocks = new int[this.n][this.n];
-        this.twin = new int[this.n][this.n];
-        this.hammingD = 0;
-        this.manhattanD = 0;
+        if (blocks == null) throw new NullPointerException();
+
+        this.n = (short) blocks.length;
+        this.blocks = new short[this.n][this.n];
 
         for (int i = 0; i < blocks.length; i++) {
             for (int j = 0; j < blocks[0].length; j++) {
-                this.blocks[i][j] = blocks[i][j];
-                this.twin[i][j] = blocks[i][j];
+                this.blocks[i][j] = (short) blocks[i][j];
                 if (this.blocks[i][j] == 0) {
-                    empRow = i;
-                    empCol = j;
-                }
-            }
-        }
-        // making the twin a 'twin'
-        this.twin[0][0] = this.blocks[0][1];
-        this.twin[0][1] = this.blocks[0][0];
-
-        // calculate Hamming and Manhattan distances now and store them
-        for (int i = 0; i < blocks.length; i++) {
-            for (int j = 0; j < blocks[0].length; j++) {
-                int currentBlock = blocks[i][j];
-                if (currentBlock != 0 && currentBlock != (i * n + j + 1)) {
-                    this.hammingD++; // current block is out of place
-                    int correctRow = Math.floorDiv(currentBlock - 1, this.n);
-                    int correctCol = (currentBlock - 1) % this.n;
-                    this.manhattanD += (Math.abs(correctRow - i) + Math.abs(correctCol - j));
+                    empRow = (short) i;
+                    empCol = (short) j;
+                } else if (this.blocks[i][j] != (i * n + j + 1)) {
+                    hammingD++; // current block is out of place
+                    int correctRow = Math.floorDiv(this.blocks[i][j] - 1, this.n);
+                    int correctCol = (this.blocks[i][j] - 1) % this.n;
+                    manhattanD += (Math.abs(correctRow - i) + Math.abs(correctCol - j));
                 }
             }
         }
@@ -67,9 +53,26 @@ public class Board {
     }
 
     public Board twin() { // a board that is obtained by exchanging any pair of blocks
-        return new Board(this.twin);
+
+        int[][] twin = new int[n][n];
+        for (int i = 0; i < blocks.length; i++) {
+            for (int j = 0; j < blocks[0].length; j++) {
+                twin[i][j] = blocks[i][j];
+            }
+        }
+
+        if (blocks[0][0] == 0 || blocks[0][1] == 0) {
+            twin[1][0] = blocks[1][1];
+            twin[1][1] = blocks[1][0];
+        } else {
+            twin[0][0] = blocks[0][1];
+            twin[0][1] = blocks[0][0];
+        }
+
+        return new Board(twin);
     }
 
+    @Override
     public boolean equals(Object y) { // does this board equal y?
         if (y == this) return true;
         if (y == null) return false;
@@ -80,20 +83,31 @@ public class Board {
                 other.manhattanD != this.manhattanD ||
                 other.hammingD != this.hammingD) return false;
 
-        return other.toString().equals(this.toString());
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (blocks[i][j] != other.blocks[i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     public Iterable<Board> neighbors() { // all neighboring boards
         Stack<Board> neighbors = new Stack<Board>();
         for (int i = empRow - 1; i <= empRow + 1; i++) {
             for (int j = empCol - 1; j <= empCol + 1; j++) {
-                if ((i == empRow ^ j == empCol) && (i < n && i >= 0) && (j < n && j >= 0)) { // check only top,left,right,bottom, only if in bounds
-                    int toSwap = blocks[i][j];
-                    blocks[i][j] = 0; // performm the swap
-                    blocks[empRow][empCol] = toSwap;
-                    neighbors.push(new Board(blocks));
-                    blocks[i][j] = toSwap; // undo the swap
-                    blocks[empRow][empCol] = 0;
+                // check only top,left,right,bottom, only if in bounds
+                if ((i == empRow ^ j == empCol) && (i < n && i >= 0) && (j < n && j >= 0)) {
+                    int[][] newBlocks = new int[n][n];
+                    for (int k = 0; k < blocks.length; k++) {
+                        for (int l = 0; l < blocks[0].length; l++) {
+                            newBlocks[k][l] = blocks[k][l];
+                        }
+                    }
+                    newBlocks[i][j] = 0; // perform the swap
+                    newBlocks[empRow][empCol] = blocks[i][j];
+                    neighbors.push(new Board(newBlocks));
                 }
             }
         }
@@ -115,14 +129,14 @@ public class Board {
     public static void main(String[] args) { // unit tests (not graded) }
         int[][] test1 = new int[3][3];
         test1[0][0] = 1;
-        test1[0][1] = 2;
-        test1[0][2] = 3;
-        test1[1][0] = 4;
+        test1[0][1] = 4;
+        test1[0][2] = 6;
+        test1[1][0] = 2;
         test1[1][1] = 5;
-        test1[1][2] = 6;
+        test1[1][2] = 3;
         test1[2][0] = 7;
-        test1[2][1] = 8;
-        test1[2][2] = 0;
+        test1[2][1] = 0;
+        test1[2][2] = 8;
 
         Board testBoard = new Board(test1);
 
--- p4_puzzle/src/Solver.java
+++ p4_puzzle/src/Solver.java
@@ -1,29 +1,114 @@
 import edu.princeton.cs.algs4.MinPQ;
+import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.StdOut;
 
 /**
  * Created by Jared Meek on 04/06/2017.
+ * dummysave
  */
 public class Solver {          // find a solution to the initial board (using the A* algorithm)
-    private int moves;
     private boolean solvable;
+    private SearchNode goalNode;
+    //private int counter;
 
+    private class SearchNode implements Comparable<SearchNode> {
+        public Board board;
+        public SearchNode previous;
+        public int movesSoFar;
+
+        public SearchNode(Board b, SearchNode p, int m) {
+            this.board = b;
+            this.previous = p;
+            this.movesSoFar = m;
+        }
+
+        public int compareTo(SearchNode orig) {
+            int comp = (board.manhattan() + this.movesSoFar) - (orig.board.manhattan() + orig.movesSoFar);
+            // if the above is zero, need to compare just manhattans to tie break
+            if (comp == 0) return board.manhattan() - orig.board.manhattan();
+            return comp;
+        }
+
+    }
 
     public Solver(Board initial) {
-        this.moves = 0;
+        if (initial == null) throw new NullPointerException();
+        MinPQ<SearchNode> queue = new MinPQ<>();
+        MinPQ<SearchNode> twinQueue = new MinPQ<>();
+        queue.insert(new SearchNode(initial,null,0));
+        twinQueue.insert(new SearchNode(initial.twin(),null,0));
+       // counter=0;
+
+        SearchNode current;
+        SearchNode twinCurrent;
 
 
+        while (true) {
+            // examine the next step
+            current = queue.delMin();
+            if (current.board.isGoal()) {
+                solvable = true;
+                goalNode = current;
+                break;
+            }
+
+            twinCurrent = twinQueue.delMin();
+            if (twinCurrent.board.isGoal()) {
+                solvable = false;
+                break;
+            }
+
+            // if not at goal, get neighbors and setup next round
+            for (Board b : current.board.neighbors()) {
+                // first check if the neighbor is the same as the board we came from, ignore those
+                if (current.previous == null || !b.equals(current.previous.board)) {
+                    queue.insert(new SearchNode(b, current, current.movesSoFar + 1));
+                }
+            }
+
+            for (Board b : twinCurrent.board.neighbors()) {
+                // first check if the neighbor is the same as the board we came from, ignore those
+                if (twinCurrent.previous == null || !b.equals(twinCurrent.previous.board)) {
+                    twinQueue.insert(new SearchNode(b, twinCurrent, twinCurrent.movesSoFar + 1));
+                }
+            }
+            /*
+            counter++;
+
+            if (counter % 100000 == 0) {
+                StdOut.println(counter);
+                StdOut.println("Next board moves: " + queue.min().movesSoFar);
+                StdOut.println("Next board ManhD: " + queue.min().board.manhattan());
+                StdOut.println("Next board priority: " + (queue.min().board.manhattan()+queue.min().movesSoFar));
+            }
+            */
+
+        }
     }
 
     public boolean isSolvable() {           // is the initial board solvable?
-
+        return solvable;
     }
 
     public int moves() {                    // min number of moves to solve initial board; -1 if unsolvable
-
+        if (isSolvable()) {
+            return goalNode.movesSoFar;
+        } else {
+            return -1;
+        }
     }
 
     public Iterable<Board> solution() {     // sequence of boards in a shortest solution; null if unsolvable
+        if (!isSolvable()) return null;
 
+        Stack<Board> path = new Stack<>();
+        SearchNode curr = goalNode;
+
+        while (curr != null) {
+            path.push(curr.board);
+            curr = curr.previous;
+        }
+        return path;
     }
 
     public static void main(String[] args) { // solve a slider puzzle (given below)
